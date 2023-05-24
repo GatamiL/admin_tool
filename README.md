dcfd655a90f26fa4cfedb93b3696c394674b426a
Gatami <gatamilight@gmail.com>
2023-05-24 19:52:40 +0500

    adding initial code of windows

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # admin_tool
-2
+This is my first app on python for real-time network monitoring.
+In this project I use next libraries:
+- tkinter;
+- pystray;
+- pillow;
---
+++ windows.py
@@ -0,0 +1,26 @@
+from pystray import MenuItem as item
+import pystray
+from PIL import Image
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Welcome")
+window.iconbitmap('image.ico')
+
+def quit_window(icon, item):
+    icon.stop()
+    window.destroy()
+
+def show_window(icon, item):
+    icon.stop()
+    window.after(0,window.deiconify)
+
+def withdraw_window():  
+    window.withdraw()
+    image = Image.open("image.ico")
+    menu = (item('Quit', quit_window), item('Show', show_window, default=True))
+    icon = pystray.Icon("name", image, "title", menu)
+    icon.run()
+
+window.protocol('WM_DELETE_WINDOW', withdraw_window)
+window.mainloop()
